cec42e6e052ec61545f636fb82c6f522a0eb07f2
Philip Colmer <philip.colmer@linaro.org>
2020-10-08 12:29:52 +0100

    Initial commit

---
+++ shared/shared_google.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Handles all interactions between the automation and Google.
+
+To avoid the need to initialise the authentication, a service
+account must be used.
+
+https://developers.google.com/identity/protocols/oauth2/service-account
+"""
+
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+import shared.globals
+
+SCOPES = [
+    'https://www.googleapis.com/auth/admin.directory.user.security',
+    'https://www.googleapis.com/auth/admin.directory.user',
+    'https://www.googleapis.com/auth/admin.directory.user.alias',
+    'https://www.googleapis.com/auth/admin.directory.group.readonly',
+    'https://www.googleapis.com/auth/admin.datatransfer',
+    'https://www.googleapis.com/auth/spreadsheets'
+]
+
+def get_credentials():
+    json_blob = shared.globals.get_google_credentials()
+    return service_account.Credentials.from_service_account_info(
+        json_blob, scopes=SCOPES)
+
+def check_group_alias(email):
+    """ See if we can find a group on Google with the specified email address. """
+    delegated_creds = get_credentials().with_subject(shared.globals.CONFIGURATION["google_admin"])
+    with build('admin', 'directory_v1', credentials=delegated_creds) as service:
+        response = service.groups().get(groupKey=email).execute()
+        if "aliases" in response:
+            # There are aliases on this group. We only care if there are
+            # aliases because the caller will already have checked LDAP,
+            # so groups without aliases will match on the LDAP test.
+            return response["email"]
+    return None
