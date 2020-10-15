a3aa45fe33516d495952dd041c5f0aaccfc64f04
Philip Colmer <philip.colmer@linaro.org>
2020-10-15 20:53:09 +0100

    Simplify validation

--- validate_config.py
+++ validate_config.py
@@ -1,14 +1,8 @@
-import json
-import os
+import shared.globals
 import sys
 
-from json_minify import json_minify
-
-basedir = os.path.dirname(__file__)
-config_file = os.path.join(basedir, "configuration.jsonc")
 try:
-    with open(config_file) as handle:
-        CONFIGURATION = json.loads(json_minify(handle.read()))
-except json.decoder.JSONDecodeError:
-    print("Configuration file is invalid")
+    shared.globals.initialise_config()
+except Exception as exc:
+    print(str(exc))
     sys.exit(1)
