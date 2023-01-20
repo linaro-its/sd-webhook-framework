1c92fc96302792f02c05849e9e085bcaf0c7116f
Prasanth Nair <prasanthcambridge@yahoo.co.uk>
2023-01-20 15:48:42 +0000

    Added 100 % testing coverage for email.py

--- tests/test_email.py
+++ tests/test_email.py
@@ -3,11 +3,8 @@
 
 import pytest
 import mock
-import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-# from moto import mock_ses
-
 from email.message import EmailMessage
 import shared.globals
 import shared.email
@@ -58,6 +55,16 @@ class MockSMTP3(BaseMockSMTPSession):
         assert from_list == "from@mock.mock"
         assert to_list == ["to@mock.mock"]
 
+class MockBoto3:
+    """ Mock boto3 client"""
+    @staticmethod
+    def send_email(Destination,
+                   Message,
+                   Source,
+                   ConfigurationSetName=None):
+        return {
+            "MessageId": "something"
+        }
 
 def mock_smtp_1(server, port):
     """ Test that send_email is passing the correct values. """
@@ -77,35 +84,6 @@ def mock_smtp_3(server, port):
     assert port == 25
     return MockSMTP3()
 
-# @mock.patch(
-#     'smtplib.SMTP',
-#     side_effect=mock_smtp,
-#     autospec=True
-# )
-# def test_send_email(mi1):
-#     """ Test send_email """
-#     shared.globals.CONFIGURATION = {}
-#     with pytest.raises(shared.email.MissingConfig):
-#         shared.email.send_email(None)
-#     shared.globals.CONFIGURATION = {
-#         "mail_host": "whatever",
-#         "mail_ssl": "True",
-#     }
-#     with pytest.raises(shared.email.BadStartTls):
-#         shared.email.send_email(None)
-#     shared.globals.CONFIGURATION = {
-#         "mail_host": "whatever",
-#         "mail_user": "mock_user",
-#         "mail_password": "mock_password",
-#     }
-#     msg = EmailMessage()
-#     msg["From"] = "from@mock.mock"
-#     msg["To"] = "to@mock.mock"
-#     msg["Cc"] = "cc@mock.mock"
-#     shared.email.send_email(msg)
-#     assert mi1.called is True
-
-
 @mock.patch(
     'smtplib.SMTP',
     side_effect=mock_smtp_1,
@@ -117,6 +95,8 @@ def test_send_email_1(mi1):
         "mail_host": "whatever",
         "mail_ssl": "True",
         "mail_mechanism" : "smtp",
+        "mail_user": "mock_user",
+        "mail_password": "mock_password"
     }
     
     msg = EmailMessage()
@@ -126,14 +106,12 @@ def test_send_email_1(mi1):
     shared.email.send_email(msg)
     assert mi1.called is True
 
-
 def test_send_email_2():
     """ Test send_email when config is missing """
     shared.globals.CONFIGURATION = {
     }
     with pytest.raises(shared.email.MissingConfig):
         shared.email.send_email(None)
-
 
 def test_send_email_3():
     """ Test send_email when mail_mechanism is non smtp"""
@@ -143,14 +121,12 @@ def test_send_email_3():
     with pytest.raises(shared.email.InvalidConfig):
         shared.email.send_email(None)
 
-
 def test_send_email_parts_1():
     "Test when config is missing"
     shared.globals.CONFIGURATION = {}
 
     with pytest.raises(shared.email.MissingConfig):
         shared.email.send_email_parts(None, None, None, None, None)
-
 
 @mock.patch(
     'smtplib.SMTP',
@@ -167,7 +143,6 @@ def test_send_email_parts_2(mock_send_email_parts_2):
     shared.email.send_email_parts("from@mock.mock", "to@mock.mock, cc@mock.mock", "Mock Subject", None, None)
     assert mock_send_email_parts_2.called is True
 
-
 @mock.patch(
     'smtplib.SMTP',
     side_effect=mock_smtp_3,
@@ -181,69 +156,95 @@ def test_send_email_parts_3(mock_send_email_parts_3):
         "mail_mechanism" : "smtp",
     }
 
-    text = "Mock Subject\Message body"
-    html = "This is the plain text body"
-
+    text = "This is the plain text body"
+    html = "This is the HTML body"
     text_body = MIMEText(text, 'plain')
     html_body = MIMEText(html, 'html')
 
     shared.email.send_email_parts("from@mock.mock", "to@mock.mock", "Mock Subject", html_body, text_body)
     assert mock_send_email_parts_3.called is True
 
+@mock.patch(
+    'smtplib.SMTP',
+    side_effect=mock_smtp_3,
+    autospec=True
+)
+def test_send_email_parts_4(mock_send_email_parts_4):
+    "Test when mail mechanism is SMTP and text body is None"
+    shared.globals.CONFIGURATION = {
+        "mail_host": "whatever",
+        "mail_ssl": "True",
+        "mail_mechanism" : "smtp",
+    }
 
-# def test_send_email_via_smtp_1():
-#     """ Test when server is None"""
-#     shared.globals.CONFIGURATION = {
-#     }
-#     with pytest.raises(shared.email.MissingConfig):
-#         shared.email.send_email_via_smtp(None)
+    html = "This is the plain text body"
+    html_body = MIMEText(html, 'html')
 
+    shared.email.send_email_parts("from@mock.mock", "to@mock.mock", "Mock Subject", html_body, None)
+    assert mock_send_email_parts_4.called is True
 
-# @mock.patch(
-#     'smtplib.SMTP',
-#     side_effect=mock_smtp_1,
-#     autospec=True
-# )
-# def test_send_email_via_smtp_2(send_email_via_smtp_2):
-#     """ Test when user credentials"""
-#     shared.globals.CONFIGURATION = {
-#         "mail_host": "whatever",
-#         "mail_ssl": "True",
-#         "mail_mechanism" : "smtp",
-#         "mail_user": "mock_user",
-#         "mail_password": "mock_password",
-#     }
-#     msg = EmailMessage()
-#     msg["From"] = "from@mock.mock"
-#     msg["To"] = "to@mock.mock"
-#     msg["Cc"] = "cc@mock.mock"
-#     shared.email.send_email_via_smtp(msg)
-#     assert send_email_via_smtp_2.called is True
+def test_send_email_parts_5():
+    "Test when mail mechanism is neither SMTP nor SES"
+    shared.globals.CONFIGURATION = {
+        "mail_mechanism" : "something",
+    }
+    with pytest.raises(shared.email.InvalidConfig):
+        shared.email.send_email_parts("from@mock.mock", "to@mock.mock", "Mock Subject", None, None)
 
+@mock.patch(
+    'shared.email.boto3.client',
+    return_value=MockBoto3,
+    autospec=True,
+)
+def test_send_email_parts_6(mock_send_email_parts_6, capsys):
+    "Test when mail mechanism is SES"
+    shared.globals.CONFIGURATION = {
+        "mail_mechanism" : "ses",
+    }
+    text = "This is the plain text body"
+    html = "This is the HTML body"
+    text_body = MIMEText(text, 'plain')
+    html_body = MIMEText(html, 'html')
 
-# @pytest.fixture(scope='module')
-# def aws_credentials():
-#     os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
-#     os.environ['AWS_SECRET_ACCESS_ID'] = 'testing'
-#     os.environ['AWS_SECURITY_TOKEN'] = 'testing'
-#     os.environ['AWS_SESSION_TOKEN'] = 'testing'
+    shared.email.send_email_parts("from@mock.mock", "to@mock.mock", "Mock Subject", html_body, text_body)
+    assert mock_send_email_parts_6.called is True
+    captured = capsys.readouterr()
+    assert captured.out == "Email sent via SES to to@mock.mock; message ID is something\n"
 
+def test_send_email_via_smtp_1():
+    """ Test when server is None"""
+    shared.globals.CONFIGURATION = {
+    }
+    with pytest.raises(shared.email.MissingConfig):
+        shared.email.send_email_via_smtp(None)
 
-# @mock_ses
-# def test_send_email_via_ses_1(aws_credentials):
-#     """ """
-#     text = "Test subject!\Message body"
-#     html = """\
-#     <html>
-#         <head></head>
-#         <body>
-#             <p>Test subject!<br>
-#             Message body<br>
-#             </p>
-#         </body>
-#     </html>
-#     """
-#     text_body = MIMEText(text, 'plain')
-#     html_body = MIMEText(html, 'html')
+@mock.patch(
+    'shared.email.boto3.client',
+    return_value=MockBoto3,
+    autospec=True,
+)
+def test_send_email_via_ses_1(mi1, capsys):
+    """Test when both ses_config_set and ses_region are specified"""
+    shared.globals.CONFIGURATION = {
+        "ses_config_set": "something",
+        "ses_region": "us-east-1",
+    }
+    shared.email.send_email_via_ses("from@mock.mock", "to@mock.mock", "Mock Subject", "html_body", "text_body")
+    assert mi1.called is True
+    captured = capsys.readouterr()
+    assert captured.out == "Email sent via SES to to@mock.mock; message ID is something\n"
 
-#     shared.email.send_email_via_ses("from@mock.mock", "to@mock.mock", "Mock Subject", html_body, text_body)
+@mock.patch(
+    'shared.email.boto3.client',
+    return_value=MockBoto3,
+    autospec=True,
+)
+def test_send_email_via_ses_2(mi1, capsys):
+    """Test when ses_config_set is None"""
+    shared.globals.CONFIGURATION = {
+        "ses_region": "us-east-1",
+    }
+    shared.email.send_email_via_ses("from@mock.mock", "to@mock.mock", "Mock Subject", "html_body", "text_body")
+    assert mi1.called is True
+    captured = capsys.readouterr()
+    assert captured.out == "Email sent via SES to to@mock.mock; message ID is something\n"
