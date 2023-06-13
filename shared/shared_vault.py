002414d6536ebd30a818a8a86f9741a596da6937
Philip Colmer <philip.colmer@linaro.org>
2023-06-13 14:55:35 +0100

    Use new vault code instead of legacy library

--- shared/shared_vault.py
+++ shared/shared_vault.py
@@ -1,13 +1,45 @@
 """ Shared code to retrieve secrets from Hashicorp Vault. """
-import vault_auth
+import boto3
+import hvac
+import requests
+
 import shared.globals
 
+def get_vault_secret(secret_path: str, iam_role: str, url: str, key: str = "pw") -> str:
+    # Assume the desired IAM role
+    sts_client = boto3.client('sts')
+    assumed_role_object = sts_client.assume_role(
+        RoleArn=iam_role,
+        RoleSessionName="AssumeRoleSession1"
+    )
+    assumed_credentials = assumed_role_object['Credentials']
+    # Authenticate to Vault and get a Vault token back
+    client = hvac.Client(url=url)
+    token = client.auth.aws.iam_login(
+        assumed_credentials['AccessKeyId'],
+        assumed_credentials['SecretAccessKey'],
+        assumed_credentials['SessionToken'])
+    # Now request the secret with that token
+    header = {
+        "X-Vault-Token": token["auth"]["client_token"]
+    }
+    response = requests.get(
+        f"{url}/v1/{secret_path}",
+        headers=header)
+    secret = response.json()
+    # Revoke the Vault token now that we're done with it.
+    requests.post(
+        f"{url}/v1/auth/token/revoke-self",
+        headers=header)
+    response.raise_for_status
+    return secret["data"][key]
 
 def get_secret(secret_path, key="pw"):
     """ Retrieve a secret from Hashicorp Vault service """
-    secret = vault_auth.get_secret(
+    secret = get_vault_secret(
         secret_path,
         iam_role=shared.globals.CONFIGURATION["vault_iam_role"],
-        url=shared.globals.CONFIGURATION["vault_server_url"]
+        url=shared.globals.CONFIGURATION["vault_server_url"],
+        key=key
     )
-    return secret["data"][key]
+    return secret
